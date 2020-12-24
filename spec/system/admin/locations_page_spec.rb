9cdbf84b3149239058bab20905c9071973ca1aa8
Colin Burn-Murdoch <colin.burn-murdoch@digital.cabinet-office.gov.uk>
2020-12-24 14:13:38 +0000

    DRY up the location value.

--- spec/system/admin/locations_page_spec.rb
+++ spec/system/admin/locations_page_spec.rb
@@ -14,23 +14,27 @@ feature "Locations Page" do
       @ips_count = find(:xpath, "//h1[@id='ips-count']")["innerText"].to_i
     end
 
+    let(:location) { "Automated Test Location" }
+    let(:postcode) { "N1" }
+
     it "adds a location" do
+
       within(".leftnav") { click_link "Locations" }
       click_link "Add a location"
-      fill_in "location[address]", with: "Automated Test Location"
-      fill_in "location[postcode]", with: "N1"
+      fill_in "location[address]", with: location
+      fill_in "location[postcode]", with: postcode
       click_button "Add location"
 
-      expect(page).to have_content "Added Automated Test Location, N1"
+      expect(page).to have_content "Added #{location}, #{postcode}"
     end
 
     it "adds an IP address" do
       within(".leftnav") { click_link "Locations" }
-      find(:xpath, "//*[contains(text(), 'Automated Test Location, N1')]/ancestor::caption/descendant::a[text()='Add IP addresses']").click
+      find(:xpath, "//*[contains(text(), '#{location}, #{postcode}')]/ancestor::caption/descendant::a[text()='Add IP addresses']").click
       fill_in "location[ips_attributes][0][address]", with: "8.8.8.8"
       click_button "Add IP addresses"
 
-      expect(page).to have_content("Added 1 IP address to Automated Test Location, N1")
+      expect(page).to have_content("Added 1 IP address to #{location}, #{postcode}")
     end
 
     it "shows the expected information on overview page" do
@@ -44,7 +48,7 @@ feature "Locations Page" do
       within(".leftnav") { click_link "Locations" }
 
       # Would prefer to select this by IP, however the additional markup around the IP is making that difficult.
-      find(:xpath, "//*[contains(text(), 'Automated Test Location, N1')]/ancestor::table/descendant::a[text()='Remove']").click
+      find(:xpath, "//*[contains(text(), '#{location}, #{postcode}')]/ancestor::table/descendant::a[text()='Remove']").click
       click_button "Remove"
 
       expect(page).to have_content "Successfully removed IP address 8.8.8.8"
@@ -53,10 +57,10 @@ feature "Locations Page" do
     it "deletes a location" do
       within(".leftnav") { click_link "Locations" }
 
-      find(:xpath, "//*[contains(text(), 'Automated Test Location, N1')]/ancestor::table/descendant::a[text()='Remove this location']").click
+      find(:xpath, "//*[contains(text(), '#{location}, #{postcode}')]/ancestor::table/descendant::a[text()='Remove this location']").click
       click_button "Yes, remove this location"
 
-      expect(page).to have_content "Successfully removed location Automated Test Location"
+      expect(page).to have_content "Successfully removed location #{location}"
     end
 
     it "shows the expected information on overview page" do

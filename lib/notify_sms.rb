3096fddcb344fb2abab443aed0ca5dc016b6c353
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2023-05-17 15:20:25 +0100

    Implement the SMS signup journey tests
    This test uses a Notify service account called GovWifi-Smoketest.
    This account is used to send and receive SMS messages similar
    to how a regular GovWifi user would
    
    Synchronisation is handled by reading the first SMS message from
    the GovWIfi-Smoketest account, then sending a 'go' message to start
    the signup process. The GovWifi-Smoketest account is then polled for
    the first message that is different from the one read earlier.

---
+++ lib/notify_sms.rb
@@ -0,0 +1,35 @@
+require_relative "../lib/services"
+module NotifySms
+  def send_go_message(phone_number:, template_id:)
+    Services.notify.send_sms(
+      phone_number:,
+      template_id:,
+    )
+  end
+
+  def read_reply_sms(phone_number:, after_id:, timeout: 50)
+    Timeout.timeout(timeout, nil, "Waited too long for signup SMS. Last received SMS is #{after_id}") do
+      normalised_phone_number = normalise(phone_number)
+      while (result = get_first_sms(phone_number: normalised_phone_number))&.id == after_id
+        print "."
+        sleep 1
+      end
+      result.content
+    end
+  end
+
+  def get_first_sms(phone_number:)
+    Services.notify.get_received_texts.collection.find { |message| message.user_number == normalise(phone_number) }
+  end
+
+  def parse_message(message)
+    match = /Username:[\n\r\s]*(?<username>[a-z]{6})[\n\r\s]*Password:[\n\r\s]*(?<password>(?:[A-Z][a-z]+){3})/.match(message)
+    [match[:username], match[:password]]
+  end
+
+private
+
+  def normalise(phone_number)
+    phone_number.delete("+").sub(/^0/, "44")
+  end
+end
